a192917abf7b657cd7a76135ecbfc537632f5cb8
Aubrey <aubrey.goodman@gmail.com>
2012-11-27 19:05:59 -0500

    Initial Commit

---
+++ BusPlusDriver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BusPlusDriver
+//
+//  Created by Aubrey Goodman on 11/27/12.
+//  Copyright (c) 2012 Migrant Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
